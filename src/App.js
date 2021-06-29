e404f23f07bf70bf1a51f542a014d31a0bc937b2
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-06-29 15:37:18 +0700

    create project
    create project ui with react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Welcome to my website
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/ThangHuynh18/rookie-web-ecommerce-ui"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My github repository
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
